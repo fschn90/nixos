15812ff283acee6cf057144dbb91fa8569b2d8bc
flo-schn <hello@fschn.org>
2024-02-01 15:05:40 +0100

    alacrity customization

--- homes/fschn/packages.nix
+++ homes/fschn/packages.nix
@@ -28,6 +28,14 @@
     gtypist
   ];
 
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.decorations = "None";
+      window.startup_mode = "Maximized";
+    };
+  };              
+
   programs.neovim = {
     enable = true;
     viAlias = true;
