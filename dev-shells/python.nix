87e8cd0aacb90e300e0c06d26cd5e57b9c937f19
fschn90 <hello@fschn.org>
2024-02-26 16:37:19 +0100

    working dev shell

--- dev-shells/python.nix
+++ dev-shells/python.nix
@@ -1,18 +1,15 @@
-# Nix expression that provides a "fish" shell with the
-# specified Python packages. 'buildFHSUserEnv' provides
-# access to the system libstdc library required by
-# Python packages like iPython or Numpy
-#
-# Run with 'nix-shell python-shell.nix'
-#
-{pkgs ? import <nixpkgs> {}}:
-(pkgs.buildFHSUserEnv {
-  name = "pipzone";
-  targetPkgs = kgs: (with pkgs; [
-    python311
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    zlib
+    python3
     python311Packages.pip
     python311Packages.virtualenv
     python311Packages.spacy
-  ]);
-  runScript = "fish";
-}).env
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.zlib}/lib"
+  '';
+}
