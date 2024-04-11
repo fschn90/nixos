ff2a35437b887f2eb730bc1917a743f831eb0ab8
fschn90 <hello@fschn.org>
2024-04-11 18:12:30 +0200

    improved syntax in sops.nix and renamed secret yaml

--- modules/sops.nix
+++ modules/sops.nix
@@ -4,7 +4,7 @@
 
   sops = {
 
-  defaultSopsFile = ../secrets/example.yaml;
+  defaultSopsFile = ../secrets/main.yaml;
   defaultSopsFormat = "yaml";
   
   age.keyFile = "/var/lib/sops-nix/key.txt";
@@ -15,40 +15,39 @@
   secrets."ssh/config" = {
     mode = "0644";
     path = "/home/fschn/.ssh/config";
-    owner = "fschn";
+    owner = config.users.users.fschn.name;
   };
  
   secrets."ssh/authorized_keys" = {
     mode = "0600";
     path = "/home/fschn/.ssh/authorized_keys";
-    owner = "fschn";
+    owner = config.users.users.fschn.name;
   };
 
   secrets."ssh/keys/hetzner_flo" = {
     mode = "0600";
     path = "/home/fschn/.ssh/hetzner_flo";
-    owner = "fschn";
+    owner = config.users.users.fschn.name;
   };
 
   secrets."ssh/keys/hetzner_flo.pub" = {
     mode = "0644";
     path = "/home/fschn/.ssh/hetzner_flo.pub";
-    owner = "fschn";
+    owner = config.users.users.fschn.name;
   };
   
   secrets."ssh/keys/id_ed25519" = {
     mode = "0600";
     path = "/home/fschn/.ssh/id_ed25519";
-    owner = "fschn";
+    owner = config.users.users.fschn.name;
   };
  
   secrets."ssh/keys/id_ed25519.pub" = {
     mode = "0644";
     path = "/home/fschn/.ssh/id_ed25519.pub"; 
-    owner = "fschn";
+    owner = config.users.users.fschn.name;
   };
   
-  secrets."myservice/my_subdir/my_secret" = {};
   secrets."Users/fschn/Password".neededForUsers = true;
   };
 }  
