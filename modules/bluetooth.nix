b3ec4f0409c1ec157c5aca91a677e20a20de56c9
fschn90 <hello@fschn.org>
2024-04-19 13:45:55 +0200

    tidying bluetooth up a bit

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -4,9 +4,15 @@
   # Enable bluetooth
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot =
-    true; # powers up the default Bluetooth controller on boot
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  # showing battery charge of bluetooth devices, doesnt seem to work tho
+  hardware.bluetooth.settings = {
+	  General = {
+		  Experimental = true;
+	  };
+  };
 
+  # Media Player Remote Interface Specification: using headphone buttons to eg play next song
   systemd.user.services.mpris-proxy = {
     description = "Mpris proxy";
     after = [ "network.target" "sound.target" ];
