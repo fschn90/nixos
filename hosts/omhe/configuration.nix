dbd74741bdd9b432b40421b2a51943ae305c3037
fschn90 <hello@fschn.org>
2024-06-03 22:06:27 +0200

    more wifi driver related settings

--- hosts/omhe/configuration.nix
+++ hosts/omhe/configuration.nix
@@ -42,13 +42,10 @@
   
     options iwlwifi 11n_disable=1 swcrypto=0 bt_coex_active=0 power_save=0
     options iwlmvm power_scheme=1 
-    options iwlwifi d0i3_disable=1 
     options iwlwifi uapsd_disable=1 
-    options iwlwifi lar_disable=1
-
   '';
   
-  
+  # networking.wireless.enable = true; 
   hardware.bluetooth.enable = false;
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
