2178a977f78df9aefb51ee2f7c3a1784c1248e74
fschn90 <hello@fschn.org>
2024-06-03 18:08:26 +0200

    wifi extramodprobe config

--- hosts/omhe/configuration.nix
+++ hosts/omhe/configuration.nix
@@ -38,6 +38,16 @@
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.hostId = "e2990a3c";
 
+  boot.extraModprobeConfig = ''
+  
+    options iwlwifi 11n_disable=1 swcrypto=0 bt_coex_active=0 power_save=0
+    options iwlmvm power_scheme=1 
+    options iwlwifi d0i3_disable=1 
+    options iwlwifi uapsd_disable=1 
+    options iwlwifi lar_disable=1
+
+  '';
+  
   # flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
