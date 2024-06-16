ad67510772c5c6b1ca0ba761f7c6adc43afd5b6a
fschn90 <hello@fschn.org>
2024-06-16 22:54:28 +0200

    adding zfs pool for omhe

--- hosts/omhe/configuration.nix
+++ hosts/omhe/configuration.nix
@@ -37,6 +37,7 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.hostId = "e2990a3c";
+  boot.zfs.extraPools = [ "tank" ];
 
   boot.extraModprobeConfig = ''
   
