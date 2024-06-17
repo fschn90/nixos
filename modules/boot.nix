c0ab06d5e1ea1a1186c7d44f9ec6a9d1bd79b3af
fschn90 <hello@fschn.org>
2024-06-17 10:47:43 +0200

    systemd-boot

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,9 +1,12 @@
+# {
+#
+#   boot.loader.grub.enable = true;
+#   boot.loader.grub.devices = [ "nodev" ];
+#   boot.loader.grub.efiInstallAsRemovable = true;
+#   boot.loader.grub.efiSupport = true;
+#   boot.loader.grub.useOSProber = true;
+#
+# }
 {
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
-
+  boot.loader.systemd-boot.enable = true;
 }
