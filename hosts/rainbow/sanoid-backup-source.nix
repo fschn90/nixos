92278377814ad006c46910fc9b8c1acea0644c0b
fschn90 <hello@fschn.org>
2024-06-19 14:23:56 +0200

    start of setting up syncoid prerequisites

--- hosts/rainbow/sanoid-backup-source.nix
+++ hosts/rainbow/sanoid-backup-source.nix
@@ -1,9 +1,27 @@
+{ config, ... }:
+
 {
 
   sops = {
 
-    secrets."ssh/keys/backup.pub" = {};
-
+    # secrets."ssh/keys/backup" = {
+    #   mode = "0604";
+    #   path = "/var/lib/syncoid/backup";
+    #   owner = config.users.users.backup.name;
+    # };
+    
+    secrets."ssh/keys/backup" = {
+      # mode = "0600";
+      # path = "/home/fschn/.ssh/backup";
+      # owner = config.users.users.fschn.name;
+    };
+   
+    secrets."ssh/keys/backup.pub" = {
+      # mode = "0644";
+      # path = "/home/fschn/.ssh/backup.pub"; 
+      # owner = config.users.users.fschn.name;
+    };   
+  
   };
 
 
@@ -16,6 +34,7 @@
       authorizedKeys.keys = [
         config.sops.secrets."ssh/keys/backup.pub".path
       ];
+      # authorizedKeys.keyFiles = [ config.sops.secrets."ssh/keys/backup.pub".path ];
     };
   };
 
