d5264b47c1356d0b5c5f15ee2c2c78be6bf394c3
fschn90 <hello@fschn.org>
2024-07-17 19:00:19 +0200

    tailscale autkeyfile update

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -9,13 +9,13 @@
   services.tailscale.enable = true;
   services.tailscale.extraUpFlags = [
     "--ssh"
+    "--auth-key=file:${config.sops.secrets."tailscale/key".path}"
   ];
   services.tailscale.authKeyFile = config.sops.secrets."tailscale/key".path; 
 
   sops.secrets."tailscale/key" = {
-    # mode = "0400";
-    # path = "/mnt/Nextcloud/Admin-Password";
-    # owner = "nextcloud";
+    mode = "0400";
+    owner = "root";
   };
   
 } 
