e0af8598f85b39143c10691913f5226f5ea320cd
fschn90 <hello@fschn.org>
2024-08-15 14:51:45 +0200

    tailscale still and again

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -7,44 +7,12 @@
   ];
 
   services.tailscale.enable = true;
-  # services.tailscale.extraUpFlags = [
-  #   "--ssh"
-  #   # "--auth-key 'file:${config.sops.secrets."tailscale/oauth".path}&preauthorized=true'"
-  # ];
-  # services.tailscale.authKeyFile = config.sops.secrets."tailscale/key".path; 
-  services.tailscale.authKeyFile = config.sops.secrets."tailscale/OAuth".path; 
+  services.tailscale.extraUpFlags = [
+    "--ssh"
+  ];
+  services.tailscale.authKeyFile = config.sops.secrets."tailscale/key".path; 
 
-  sops.secrets."tailscale/OAuth" = {
-    # mode = "0400";
-    # owner = "root";
+  sops.secrets."tailscale/key" = {
   };
 
-  # systemd.services.tailscale-autoconnect = {
-  #   description = "Automatic connection to Tailscale";
-  #
-  #   # make sure tailscale is running before trying to connect to tailscale
-  #   after = [ "network-pre.target" "tailscale.service" ];
-  #   wants = [ "network-pre.target" "tailscale.service" ];
-  #   wantedBy = [ "multi-user.target" ];
-  #
-  #   # set this service as a oneshot job
-  #   serviceConfig.Type = "oneshot";
-  #
-  #   # have the job run this shell script
-  #   script = with pkgs; ''
-  #     # wait for tailscaled to settle
-  #     sleep 2
-  #
-  #     # check if we are already authenticated to tailscale
-  #     status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-  #     if [ $status = "Running" ]; then # if so, then do nothing
-  #       exit 0
-  #     fi
-  #
-  #     # otherwise authenticate with tailscale
-  #     ${tailscale}/bin/tailscale up -authkey file:${config.sops.secrets."tailscale/key".path}
-  #   '';
-  # };
-  #
-  
 } 
