e1de2b7086a8e20f360852541cea9f09db91141e
fschn90 <hello@fschn.org>
2024-08-23 12:52:50 +0200

    improving prometheus scraper config

--- modules/prometheus-scraper.nix
+++ modules/prometheus-scraper.nix
@@ -7,23 +7,16 @@
 
       scrapeConfigs = [
         {
-          job_name = "node_omhe";
+          job_name = "node";
           static_configs = [{
-            targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+            targets = [ 
+              "omhe:${toString config.services.prometheus.exporters.node.port}" 
+              "rainbow:${toString config.services.prometheus.exporters.node.port}" 
+              "oide:${toString config.services.prometheus.exporters.node.port}" 
+            ];
           }];
         }
-        {
-          job_name = "node_oide";
-          static_configs = [{
-            targets = [ "100.79.181.86:${toString config.services.prometheus.exporters.node.port}" ];
-          }];
-        }
-        {
-          job_name = "node_rainbow";
-          static_configs = [{
-            targets = [ "100.114.14.104:${toString config.services.prometheus.exporters.node.port}" ];
-          }];
-        }
+
       ];
     };
 }
