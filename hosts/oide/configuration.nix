18beb389c87d361da77ec95ce28a51c40ebb9e7e
fschn90 <hello@fschn.org>
2024-09-20 19:46:26 +0200

    workaround nvidia driver crashes linux kernel 6.10 build

--- hosts/oide/configuration.nix
+++ hosts/oide/configuration.nix
@@ -41,6 +41,16 @@
   # Set your time zone.
   time.timeZone = "Europe/Vienna";
 
+  # 20.09.2024 to avoid https://discourse.nixos.org/t/unable-to-build-nix-due-to-nvidia-drivers-due-or-kernel-6-10/49266/1
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+  version = "555.58.02";
+  sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+  sha256_aarch64 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+  openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+  settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+  persistencedSha256 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+};
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
