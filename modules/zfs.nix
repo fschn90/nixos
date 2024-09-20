a708596d98a13aae2cc3fef7bcb3b2cf6ec6dd4e
fschn90 <hello@fschn.org>
2024-09-20 16:38:34 +0200

    kernel bug temp fix undone, not needed anymore

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -5,8 +5,8 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.requestEncryptionCredentials = true;
   # temp bugfix
-  boot.kernelPackages = pkgs.linuxPackages_6_10;
-  # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  # boot.kernelPackages = pkgs.linuxPackages_6_10;
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
 
