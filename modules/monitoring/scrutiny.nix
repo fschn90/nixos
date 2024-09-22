ce2355292c0655798a50a1bf5b94db491eed74cb
fschn90 <hello@fschn.org>
2024-09-22 17:00:02 +0200

    new monitoring structure

---
+++ modules/monitoring/scrutiny.nix
@@ -0,0 +1,18 @@
+{ config, ... }:
+ 
+{
+
+  services.scrutiny.enable = true;
+  services.scrutiny.collector.enable = true;
+  services.scrutiny.settings.web.listen.port = 8080;
+
+  services.nginx = {
+    virtualHosts."scrutiny-${builtins.toString config.networking.hostName}.fschn.org" = {
+      useACMEHost = "fschn.org";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:${builtins.toString config.services.scrutiny.settings.web.listen.port}";
+      };
+    };
+  };
+}
