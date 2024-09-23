a67a766ac4a170d1409a977ca0eb9e29abd75e76
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-09-23 15:27:06 +0200

    helix editor settings

--- homes/home-modules/helix.nix
+++ homes/home-modules/helix.nix
@@ -17,11 +17,32 @@
         line-number = "relative";
         lsp.display-messages = true;
         lsp.display-inlay-hints = true;
+        mouse = false;
+        bufferline = "multiple";
+        cursorline = true;
+        color-modes = true;
+        soft-wrap.enable = true;
+        # error msg: Bad config: invalid type: map, expected a boolean ;in `auto-save`
+        # auto-save = {
+        #   focus-lost = true;
+        #   after-delay.enable = true;
+        # };
+        file-picker = {
+          hidden = false;
+        };
+        statusline = {
+          mode.normal = "";
+          mode.insert = "I";
+          mode.select = "S";
+        };
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
       };
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
+      keys.normal = {
+        esc = [ "collapse_selection" "keep_primary_selection" ];
       };
     };
     languages.language = [{
@@ -33,3 +54,4 @@
   };
 
 }
+
