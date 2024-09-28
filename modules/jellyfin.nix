d032ba00a9c1f9f4d68e6250ff1b98e21259483b
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-09-28 14:29:42 +0200

    jellyfin bug / typo fixed

--- modules/jellyfin.nix
+++ modules/jellyfin.nix
@@ -3,7 +3,7 @@
 {
 
   services.jellyfin.enable = true;
-  services.jellyfin.dataDir = "/tank/Jellygin";
+  services.jellyfin.dataDir = "/tank/Jellyfin";
   environment.systemPackages = [
     pkgs.jellyfin
     pkgs.jellyfin-web
@@ -17,4 +17,26 @@
       proxyPass = "http://localhost:8096";
     };
   };
+
+  nixpkgs.overlays = with pkgs; [
+    (
+      final: prev:
+        {
+          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
+            installPhase = ''
+              runHook preInstall
+
+              # this is the important line
+              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+              mkdir -p $out/share
+              cp -a dist $out/share/jellyfin-web
+
+              runHook postInstall
+            '';
+          });
+        }
+    )
+  ];
+
 }
