ec7fe61453dc4454e164f3b0c615d80a15b480fc
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-10-02 20:50:22 +0200

    sanoid autosnap for /tank/Deluge

--- modules/sanoid-backup-target.nix
+++ modules/sanoid-backup-target.nix
@@ -74,6 +74,15 @@
         monthly = 12;
         yearly = 2;
       };
+      "tank/Deluge" = {
+        autoprune = true;
+        autosnap = true;
+        hourly = 24;
+        daily = 31;
+        weekly = 7;
+        monthly = 12;
+        yearly = 2;
+      };
     };
     extraArgs = [ "--debug" ];
   };
