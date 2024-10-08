8304247b12c2890c27047b7fc920e34ad35b8e0a
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-10-08 11:59:05 +0200

    deploying deluge on seperate network namespace for vpn

--- modules/deluge.nix
+++ modules/deluge.nix
@@ -4,8 +4,8 @@
 
   services.deluge = {
     enable = true;
-    # dataDir = "/tank/Deluge";
-    # authFile = config.sops.secrets."Deluge/Admin".path;
+    dataDir = "/tank/Deluge";
+    authFile = config.sops.secrets."Deluge/Admin".path;
     # declarative = true;
     # config = 
     #   {
@@ -19,64 +19,95 @@
     web.enable = true;
   };
 
-  # sops.secrets."Deluge/Admin" = {
-  #   path = "/tank/Deluge/authFile";
-  #   owner = "deluge";
-  #   mode = "0440";
-  # };
+  sops.secrets."Deluge/Admin" = {
+    path = "/tank/Deluge/authFile";
+    owner = "deluge";
+    mode = "0440";
+  };
 
-  # services.nginx = {
-  #   virtualHosts."deluge.fschn.org" = {
-  #     useACMEHost = "fschn.org";
-  #     forceSSL = true;
-  #     locations."/" = {
-  #       proxyPass = "http://localhost:${toString config.services.deluge.web.port}";
-  #     };
-  #   };
-  # };
+  services.nginx = {
+    virtualHosts."deluge.fschn.orG" = {
+      useACMEHost = "fschn.org";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localHost:${toString config.services.deluge.web.port}";
+      };
+    };
+  };
 
 
-  # sops.secrets."networking/system-connections/wg-BE-44-P2P.conf" = {
-  #   mode = "0600";
-  #   path = "/etc/NetworkManager/system-connections/wg-BE-44-P2P.conf";
-  # };
+  # VPN wireguard conf file
+  sops.secrets."Deluge/vpn.conf" = { };
+  sops.secrets."Deluge/vpn-ip4addr-cidr" = { };
 
-  # systemd.services."netns@" = {
-  #   description = "%I network namespace";
-  #   before = [ "network.target" ];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     RemainAfterExit = true;
-  #     ExecStart = "${pkgs.iproute}/bin/ip netns add %I";
-  #     ExecStop = "${pkgs.iproute}/bin/ip netns del %I";
-  #   };
-  # };
+  # creating network namespace
+  systemd.services."netns@" = {
+    description = "%I network namespace";
+    before = [ "network.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.iproute}/bin/ip netns add %I";
+      ExecStop = "${pkgs.iproute}/bin/ip netns del %I";
+    };
+  };
 
-  # systemd.services.wg = {
-  #   description = "wg network interface";
-  #   bindsTo = [ "netns@wg.service" ];
-  #   requires = [ "network-online.target" ];
-  #   after = [ "netns@wg.service" ];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     RemainAfterExit = true;
-  #     ExecStart = with pkgs; writers.writeBash "wg-up" ''
-  #       set -e
-  #       ${iproute}/bin/ip link add wg0 type wireguard
-  #       ${iproute}/bin/ip link set wg0 netns wg
-  #       ${iproute}/bin/ip -n wg address add <ipv4 VPN addr/cidr> dev wg0
-  #       ${iproute}/bin/ip -n wg -6 address add <ipv6 VPN addr/cidr> dev wg0
-  #       ${iproute}/bin/ip netns exec wg \
-  #         ${wireguard-tools}/bin/wg setconf wg0 ${toString config.sops.secrets."networking/system-connections/wg-BE-44-P2P.conf".path}
-  #       ${iproute}/bin/ip -n wg link set wg0 up
-  #       ${iproute}/bin/ip -n wg route add default dev wg0
-  #       ${iproute}/bin/ip -n wg -6 route add default dev wg0
-  #     '';
-  #     ExecStop = with pkgs; writers.writeBash "wg-down" ''
-  #       ${iproute}/bin/ip -n wg route del default dev wg0
-  #       ${iproute}/bin/ip -n wg -6 route del default dev wg0
-  #       ${iproute}/bin/ip -n wg link del wg0
-  #     '';
-  #   };
-  # };
+  # setting up wireguard interafe within network namespace
+  systemd.services.wg = {
+    description = "wg network interface";
+    bindsTo = [ "netns@wg.service" ];
+    requires = [ "network-online.target" ];
+    after = [ "netns@wg.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = with pkgs; writers.writeBash "wg-up" ''
+        see -e
+        ${iproute}/bin/ip link add wg0 type wireguard
+        ${iproute}/bin/ip link set wg0 netns wg
+        ${iproute}/bin/ip -n wg address add ${toString config.sops.secrets."Deluge/vpn-ip4addr-cidr".path} dev wg0
+        # ${iproute}/bin/ip -n wg -6 address add <ipv6 VPN addr/cidr> dev wg0
+        ${iproute}/bin/ip netns exec wg \
+          ${wireguard-tools}/bin/wg setconf wg0 ${toString config.sops.secrets."Deluge/vpn.conf".path}
+        ${iproute}/bin/ip -n wg link set wg0 up
+        # need to set lo up as network namespace is started with lo down
+        ${iproute}/bin/ip -n wg link set lo up
+        ${iproute}/bin/ip -n wg route add default dev wg0
+        # ${iproute}/bin/ip -n wg -6 route add default dev wg0
+      '';
+      ExecStop = with pkgs; writers.writeBash "wg-down" ''
+        ${iproute}/bin/ip -n wg route del default dev wg0
+        # ${iproute}/bin/ip -n wg -6 route del default dev wg0
+        ${iproute}/bin/ip -n wg link del wg0
+      '';
+    };
+  };
+
+  # binding deluged to network namespace
+  systemd.services.deluged.bindsTo = [ "netns@wg.service" ];
+  systemd.services.deluged.requires = [ "network-online.target" "wg.service" ];
+  systemd.services.deluged.serviceConfig.NetworkNamespacePath = [ "/var/run/netns/wg" ];
+
+  # allowing delugeweb to access deluged in network namespace, a socket is necesarry 
+  systemd.sockets."proxy-to-deluged" = {
+    enable = true;
+    description = "Socket for Proxy to Deluge Daemon";
+    listenStreams = [ "58846" ];
+    wantedBy = [ "sockets.target" ];
+  };
+
+  # creating proxy service on socket, which forwards the same port from the root namespace to the isolated namespace
+  systemd.services."proxy-to-deluged" = {
+    enable = true;
+    description = "Proxy to Deluge Daemon in Network Namespace";
+    requires = [ "deluged.service" "proxy-to-deluged.socket" ];
+    after = [ "deluged.service" "proxy-to-deluged.socket" ];
+    unitConfig = { JoinsNamespaceOf = "deluged.service"; };
+    serviceConfig = {
+      User = "deluge";
+      Group = "deluge";
+      ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:58846";
+      PrivateNetwork = "yes";
+    };
+  };
 }
