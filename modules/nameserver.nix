7c06dec93e8240e2528cd66e2c01449e8e7ec2bd
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-10-13 20:56:15 +0200

    removing nameserver.nix

--- modules/nameserver.nix
+++
@@ -1,7 +0,0 @@
-{
-
-  # networking.networkmanager.appendNameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
-  # networking.dhcpcd.extraConfig = "nohook resolv.conf";
-  # networking.networkmanager.dns = "none";
-
-}
