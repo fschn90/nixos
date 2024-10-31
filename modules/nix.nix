39ad4479e821dd5783f23edc0ea6009864baee11
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-10-31 23:55:12 +0100

    nix gc options

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,6 +5,7 @@
   # nix store garbage collection
   nix.gc.automatic = true;
   nix.gc.dates = "weekly";
+  nix.gc.options = "--delete-older-than 30d";
   nix.optimise.automatic = true;
-  nix.optimise.dates = ["weekly"];
+  nix.optimise.dates = [ "weekly" ];
 }
