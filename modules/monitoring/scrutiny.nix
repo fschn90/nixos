72efe04d5daf8a1c6d94bd17487a43bfd7d5b2cd
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-11-28 17:44:18 +0100

    scrutiny change port to 8081 as onlyoffice needs previouos port

--- modules/monitoring/scrutiny.nix
+++ modules/monitoring/scrutiny.nix
@@ -1,10 +1,10 @@
 { config, ... }:
- 
+
 {
 
   services.scrutiny.enable = true;
   services.scrutiny.collector.enable = true;
-  services.scrutiny.settings.web.listen.port = 8080;
+  services.scrutiny.settings.web.listen.port = 8081;
 
   services.nginx = {
     virtualHosts."scrutiny-${builtins.toString config.networking.hostName}.fschn.org" = {
