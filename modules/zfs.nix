77c7dc26c9aae6657cc1eb362d6a5dfc74e14465
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-12-04 22:38:28 +0100

    latestKernelPackage for zfs workaround

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -1,4 +1,26 @@
-{ pkgs, config, ... }:
+{ lib, pkgs, config, ... }:
+
+
+let
+  isUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
+  zfsCompatibleKernelPackages = lib.filterAttrs
+    (
+      name: kernelPackages:
+        (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+        && (builtins.tryEval kernelPackages).success
+        && (
+          (!isUnstable && !kernelPackages.zfs.meta.broken)
+          || (isUnstable && !kernelPackages.zfs_unstable.meta.broken)
+        )
+    )
+    pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+
+in
 
 {
 
@@ -6,7 +28,11 @@
   boot.zfs.requestEncryptionCredentials = true;
   # temp bugfix
   # boot.kernelPackages = pkgs.linuxPackages_6_10;
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  ## option depreciated
+  # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+
+  boot.kernelPackages = latestKernelPackage;
+
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
 
