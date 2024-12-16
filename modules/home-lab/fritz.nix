59b5866c7847bbea2c4466114c8444a734a66cc4
fschn90 <79014934+fschn90@users.noreply.github.com>
2024-12-16 18:36:04 +0100

    reverse proxy for fritzbox

---
+++ modules/home-lab/fritz.nix
@@ -0,0 +1,12 @@
+{
+  services.nginx = {
+    virtualHosts."fritzbox.fschn.org" = {
+      useACMEHost = "fschn.org";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://192.168.178.1";
+      };
+    };
+  };
+
+}
