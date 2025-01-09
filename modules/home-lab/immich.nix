6fb47d20fdee1de30e33af5cb69ca26e29d5e700
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-01-09 14:16:53 +0100

    immich env

--- modules/home-lab/immich.nix
+++ modules/home-lab/immich.nix
@@ -5,6 +5,13 @@
     enable = true;
     environment.IMMICH_MACHINE_LEARNING_URL = "http://localhost:3003";
     mediaLocation = "/tank/Immich";
+    environment = {
+      UPLOAD_LOCATION = "/tank/Immich/upload";
+      THUMB_LOCATION = "/tank/Immich/thumbs";
+      ENCODED_VIDEO_LOCATION = "/tank/Immich/encoded-video";
+      PROFILE_LOCATION = "/tank/Immich/profile";
+      BACKUP_LOCATION = "/tank/Immich/backups";
+    };
   };
 
   services.nginx.virtualHosts."photos.fschn.org" = {
