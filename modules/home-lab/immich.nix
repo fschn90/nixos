9340070fbf37823517bd26713b63ae93d161b785
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-02-07 23:58:41 +0100

    nextcloud sync for phone photos

--- modules/home-lab/immich.nix
+++ modules/home-lab/immich.nix
@@ -23,4 +23,38 @@
   };
 
   users.users.immich.extraGroups = [ "users" ];
+
+
+  ### nextcloud sync for photos on phone
+
+  ### DRAFT still need to change everything 
+
+  home-manager.users.myuser = {
+    systemd.user = {
+      services.nextcloud-autosync = {
+        Unit = {
+          Description = "Auto sync Nextcloud";
+          After = "network-online.target";
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --path /music /home/myuser/music https://nextcloud.example.org";
+          TimeoutStopSec = "180";
+          KillMode = "process";
+          KillSignal = "SIGINT";
+        };
+        Install.WantedBy = [ "multi-user.target" ];
+      };
+      timers.nextcloud-autosync = {
+        Unit.Description = "Automatic sync files with Nextcloud when booted up after 5 minutes then rerun every 60 minutes";
+        Timer.OnBootSec = "5min";
+        Timer.OnUnitActiveSec = "60min";
+        Install.WantedBy = [ "multi-user.target" "timers.target" ];
+      };
+      startServices = true;
+    };
+  };
+
+
+
 }
