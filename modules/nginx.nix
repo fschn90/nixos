84d519ea115058cc7d00c794a54e0bbed0613324
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-03-06 23:43:00 +0100

    nginx.tailscaleAuth first attempt

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -6,5 +6,13 @@
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
     # logError = "stderr debug";
+    tailscaleAuth.virtualHosts = [
+      "adguard.fschn.org"
+      "monitor.fschn.org"
+      "photos.fschn.org"
+      "jellyfin.fschn.org"
+      "cloud.fschn.org"
+    ];
+    tailscaleAuth.enable = true;
   };
 }
