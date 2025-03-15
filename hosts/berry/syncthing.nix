8319ae3bd272217a98a8a79066dbb06779df6a88
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-03-15 17:08:28 +0100

    berry syncthing /etc/nixos

---
+++ hosts/berry/syncthing.nix
@@ -0,0 +1,80 @@
+{ pkgs, config, ... }:
+
+{
+
+  services.syncthing = {
+    enable = true;
+    # guiAddress = "omhe:8384";
+    guiAddress = "100.65.150.91:8384";
+    # user = config.users.users.fschn.name;
+    user = "fschn";
+    group = "users";
+    dataDir = "/home/fschn";
+    openDefaultPorts = true;
+    settings.gui = {
+      user = "fschn";
+      # password = "mypassword";
+    };
+    overrideDevices = true;
+    overrideFolders = true;
+    settings.options = {
+      globalAnnounceEnabled = false;
+      localAnnounceEnabled = true;
+      urAccepted = -1;
+    };
+    settings = {
+      devices = {
+        "omhe" = {
+          id = "5JOBVZM-DSBMHPK-MY5LAOS-OLBTSLW-EUHASPW-RJA4KIC-QHSBYPV-VEBSNQW";
+          address = "tcp://100.106.245.44:220000";
+        };
+        "rainbow" = {
+          id = "MSBKHEW-54C7Z5L-O65KLIJ-PCQZW72-C4ODMY3-SE4IXQQ-KWI2LPU-5YTVNQW";
+          address = "tcp://100.114.14.104:220000";
+        };
+      };
+      folders = {
+        # "PAPERLESS-CONSUME" = {
+        #   path = "/home/fschn/PAPERLESS-CONSUME";
+        #   devices = [ "omhe" ];
+        #   syncOwnership = true;
+        #   sendOwnership = true;
+        # };
+        # "Photos" = {
+        #   path = "/home/fschn/Pictures";
+        #   devices = [ "omhe" ];
+        #   syncOwnership = true;
+        #   sendOwnership = true;
+        # };
+        "NixOS" = {
+          path = "/etc/nixos/";
+          devices = [ "omhe" ];
+          syncOwnership = true;
+          sendOwnership = true;
+        };
+      };
+    };
+  };
+
+
+  services.nginx = {
+    virtualHosts."syncthing-${builtins.toString config.networking.hostName}.fschn.org" = {
+      useACMEHost = "fschn.org";
+      forceSSL = true;
+      locations."/" = {
+        # proxyPass = "http://localhost:${builtins.toString config.services.scrutiny.settings.web.listen.port}";
+        # proxyPass = "http://localhost:8384;
+        # proxyPass = "http://127.0.0.1:8384/";
+        proxyPass = "http://100.65.150.91:8384";
+      };
+    };
+  };
+
+  # users.users.syncthing.extraGroups = [ "users" ];
+  # users.users.fschn.extraGroups = [ "syncthing" ];
+  # systemd.services.syncthing.serviceConfig.UMask = "0007";
+
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
+
+}
+
