6bb3488e7fcfab92ab23db4fe98aaa7930dab541
Florian Schneider <79014934+fschn90@users.noreply.github.com>
2025-04-28 15:32:29 +0200

    flake.nix adding nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.11";
