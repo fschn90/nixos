d54a2e8924b426856bd208d7057af9cac0207e76
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-05-19 09:36:33 +0200

    overlays updated, removed linux firmware after network driver was fixed upsteam and now works again

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -1,19 +1,12 @@
 { inputs, pkgs, ... }:
 {
   nixpkgs.overlays = [
-    (final: _: prev: {
+    (final: _: {
       # this allows you to access `pkgs.unstable` anywhere in your config
       unstable = import inputs.nixpkgs-unstable {
         inherit (final.stdenv.hostPlatform) system;
         inherit (final) config;
       };
-      # linux-firmware = prev.linux-firmware.overrideAttrs rec {
-      #   version = "20250311";
-      #   src = prev.fetchzip {
-      #     url = "https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${version}.tar.xz ";
-      #     hash = "sha256-ZM7j+kUpmWJUQdAGbsfwOqsNV8oE0U2t6qnw0b7pT4g=";
-      #   };
-      # };
     })
   ];
 }
