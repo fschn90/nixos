8715ddb6d703d611008235b26da9b9e668e2ba90
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-06-04 21:36:35 +0200

    zfs code clean up

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -1,17 +1,12 @@
 { lib, pkgs, config, ... }:
 
-
 let
-  isUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
   zfsCompatibleKernelPackages = lib.filterAttrs
     (
       name: kernelPackages:
         (builtins.match "linux_[0-9]+_[0-9]+" name) != null
         && (builtins.tryEval kernelPackages).success
-        && (
-          (!isUnstable && !kernelPackages.zfs.meta.broken)
-          || (isUnstable && !kernelPackages.zfs_unstable.meta.broken)
-        )
+        && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
     )
     pkgs.linuxKernel.packages;
   latestKernelPackage = lib.last (
@@ -19,18 +14,12 @@ let
       builtins.attrValues zfsCompatibleKernelPackages
     )
   );
-
 in
 
 {
 
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.requestEncryptionCredentials = true;
-  # temp bugfix
-  # boot.kernelPackages = pkgs.linuxPackages_6_10;
-  ## option depreciated
-  # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-
   boot.kernelPackages = latestKernelPackage;
 
   services.zfs.autoScrub.enable = true;
