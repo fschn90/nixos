f80ef4618f349b2285408962611813866383f90e
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-06-04 21:36:56 +0200

    protonmail-bridge for omhe first attempt

---
+++ hosts/omhe/protonmail-bridge.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  # services.gnome.gnome-keyring.enable = true;
+  # security.pam.services.gdm.enableGnomeKeyring = true;
+  # programs.seahorse.enable = true;
+
+  systemd.user.services.protonmail-bridge = {
+    description = "Protonmail Bridge";
+    enable = true;
+    script = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --no-window --noninteractive --log-level info";
+    # path = [ pkgs.gnome-keyring ]; # HACK: https://github.com/ProtonMail/proton-bridge/issues/176          
+    path = [ pkgs.pass ]; # HACK: https://github.com/ProtonMail/proton-bridge/issues/176          
+    # wantedBy = [ "graphical-session.target" ];
+    # partOf = [ "graphical-session.target" ];
+    after = [ "network.target" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    pass
+  ];
+}
