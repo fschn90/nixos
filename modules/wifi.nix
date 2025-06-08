96f78d9d2ccbe1f9dbb5ca97ea31877012143b5a
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-06-08 17:18:17 +0200

    wifi phone hotspot added

--- modules/wifi.nix
+++ modules/wifi.nix
@@ -3,6 +3,7 @@
 
   networking.wireless.secretsFile = config.sops.secrets."wireless".path;
   networking.wireless.networks."FRITZ!Box 6660 Cable TA".psk = "ext:psk_home";
+  networking.wireless.networks."pixel_4817".psk = "ext:psk_hotspot";
 
   sops.secrets."wireless" = { };
 
