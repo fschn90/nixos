881308c59f57f22736a888ce11f538826c2db3fb
Florian Schneider <79014934+fschn90@users.noreply.github.com>
2025-07-09 16:56:22 +0200

    tailscale add ips to config in a better way

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,6 +1,14 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 
 {
+
+  options = {
+    tailscale.clients.omhe.ip = lib.mkOption {
+      type = lib.types.str;
+      default = "100.106.245.44";
+      # add description and use builin.readFile to link to new ip, deployed via sops-nix
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     tailscale
