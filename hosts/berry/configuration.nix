846c4485e9f80cfa4d7c5e61323e1fb141cb8f19
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-07-12 14:46:48 +0200

    berry config cleanup

--- hosts/berry/configuration.nix
+++ hosts/berry/configuration.nix
@@ -1,46 +1,22 @@
 { config, pkgs, lib, ... }:
 
-# let
-#   # user = "guest";
-#   # password = "guest";
-#   # SSID = "mywifi";
-#   # SSIDpassword = "mypassword";
-#   # interface = "wlan0";
-#   # hostname = "myhostname";
-# in {
 {
-
-
   imports =
     [
-      # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # ../../modules/systemPackages.nix
       ../../modules/wireguard.nix
       ../../modules/ssh.nix
       ../../modules/users.nix
       ../../modules/sops.nix
       ../../modules/tailscale.nix
-      # ../../modules/sanoid-backup-target.nix
       ../../modules/nix.nix
-      # ../../modules/home-lab/nextcloud.nix
       ./adguardhome.nix
       ../../modules/acme.nix
-      # ../../modules/home-lab/jellyfin.nix
       ../../modules/nginx.nix
-      # ../../modules/monitoring/default-server.nix
-      # ../../modules/monitoring/default-workstation.nix
       ../../modules/monitoring/prometheus-exporters.nix
       ../../modules/monitoring/promtail.nix
-      # ../../modules/home-lab/deluge.nix
-      # ../../modules/home-lab/firefox-sync.nix
-      # ../../modules/home-lab/immich.nix
-      # ../../modules/home-lab/paperless.nix
-      # ../../modules/home-lab/syncthing.nix
-      # ../../modules/home-lab/fritz.nix
       ./tor.nix
     ];
-
 
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
@@ -60,30 +36,13 @@
   };
 
   networking = {
-    # hostName = hostname;
     hostName = "berry";
-    # wireless = {
-    #   enable = true;
-    #   networks."${SSID}".psk = SSIDpassword;
-    #   interfaces = [ interface ];
-    # };
   };
-
-  # environment.systemPackages = with pkgs; [ vim ];
 
   # autologin of user
   services.getty.autologinUser = config.users.users.fschn.name;
 
   services.openssh.enable = true;
-
-  # users = {
-  #   mutableUsers = false;
-  #   users."${user}" = {
-  #     isNormalUser = true;
-  #     password = password;
-  #     extraGroups = [ "wheel" ];
-  #   };
-  # };
 
   # allow unfree
   nixpkgs.config.allowUnfree = true;
@@ -114,7 +73,7 @@
         useACMEHost = "fschn.org";
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://100.65.150.91:3000";
+          proxyPass = "http://${toString config.tailnet.berry}:3000";
         };
       };
       "jellyfin.fschn.org" = {
@@ -128,28 +87,6 @@
   };
   networking.firewall.allowedTCPPorts = [ 53 2222 2223 45849 ];
   networking.firewall.allowedUDPPorts = [ 53 2222 2223 45849 ];
-
-  networking.nat = {
-    enable = true;
-    internalInterfaces = [ "tailscale0" ];
-    externalInterface = "tailscale0";
-    forwardPorts = [
-      {
-        sourcePort = 2223;
-        proto = "tcp";
-        destination = "100.114.14.104:8096";
-      }
-    ];
-  };
-
-
-  # services.nginx.virtualHosts."jellyfin.fschn.org" = {
-  # forceSSL = true;
-  # useACMEHost = "fschn.org";
-  # locations."/" = {
-  # proxyPass = "http://localhost:2223";
-  # };
-  # };
 
   hardware.enableRedistributableFirmware = true;
   system.stateVersion = "24.11";
