99f7891cec1025d0db6716b2c032d1bdd64bb6a5
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-09-20 22:36:27 +0200

    privoxy with protonvpn interface for prowlarr

--- hosts/omhe/arr.nix
+++ hosts/omhe/arr.nix
@@ -23,4 +23,30 @@
   # allowing access to indexers behing cloudflare
   services.flaresolverr.enable = true;
 
+  # binding privoxy to protonvpn network namespace
+  services.privoxy.enable = true;
+  systemd.services.privoxy.bindsTo = [ "netns@wg.service" ];
+  systemd.services.privoxy.requires = [ "network-online.target" "wg.service" ];
+  systemd.services.privoxy.serviceConfig.NetworkNamespacePath = [ "/var/run/netns/wg" ];
+
+  # to access privoxy from the root namespace, a socket is necesarry
+  systemd.sockets."proxy-to-privoxy" = {
+    enable = true;
+    description = "Socket for Proxy to Deluge Daemon";
+    listenStreams = [ "8118" ];
+    wantedBy = [ "sockets.target" ];
+  };
+
+  # creating proxy service on socket, which forwards the same port from the root namespace to the isolated namespace
+  systemd.services."proxy-to-privoxy" = {
+    enable = true;
+    description = "Proxy to Privoxy in Network Namespace";
+    requires = [ "privoxy.service" "proxy-to-privoxy.socket" ];
+    after = [ "privoxy.service" "proxy-to-privoxy.socket" ];
+    unitConfig = { JoinsNamespaceOf = "privoxy.service"; };
+    serviceConfig = {
+      ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:8118";
+      PrivateNetwork = "yes";
+    };
+  };
 }  
