c1643421305ba467e0f0cf6a1efa3b0a154af227
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-10-02 14:32:28 +0200

    wled reverse proxy

--- hosts/omhe/nginx-reverse-proxies.nix
+++ hosts/omhe/nginx-reverse-proxies.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   services.nginx = {
     virtualHosts = {
@@ -8,7 +9,25 @@
           proxyPass = "http://192.168.178.1";
         };
       };
+      "wled.fschn.org" = {
+        useACMEHost = "fschn.org";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://192.168.178.147";
+        };
+      };
     };
   };
 
+
+  services.adguardhome.settings.filtering.rewrites = [
+    {
+      domain = "firtzbox.fschn.org";
+      answer = "${toString config.tailnet.omhe}";
+    }
+    {
+      domain = "wled.fschn.org";
+      answer = "${toString config.tailnet.omhe}";
+    }
+  ];
 }
