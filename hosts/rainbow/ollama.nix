b1da1a4d53726d52e133744b0c2aa2369f6faa62
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-11-08 10:32:22 +0100

    ollama models reduced

--- hosts/rainbow/ollama.nix
+++ hosts/rainbow/ollama.nix
@@ -9,7 +9,7 @@
     # HCC_AMDGPU_TARGET = "gfx1101"; # used to be necessary, but doesn't seem to anymore
     # };
     rocmOverrideGfx = "11.0.1";
-    loadModels = [ "gemma3:12b-it-q8_0" "llama3.1:latest" "deepseek-r1:14b" ];
+    loadModels = [ "gemma3:12b-it-q8_0" ];
     host = "0.0.0.0";
   };
 
