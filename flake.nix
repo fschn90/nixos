eb495dad84db73b7dbd5ee7e70651b2cb31812a7
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-12-15 23:09:38 +0100

    home-manager bump up to 25.11

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # hardware specific
