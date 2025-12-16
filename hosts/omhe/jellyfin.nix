219e0a6f321aca099b0b8ebe69d548db8616c4ad
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-12-16 20:41:04 +0100

    jellyfin remove overlay for skip-intro plugin

--- hosts/omhe/jellyfin.nix
+++ hosts/omhe/jellyfin.nix
@@ -21,25 +21,5 @@
   # making sure jellyfin has access to deluge download dir
   users.users.jellyfin.extraGroups = [ "deluge" ];
 
-  nixpkgs.overlays = with pkgs; [
-    (
-      final: prev:
-        {
-          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
-            installPhase = ''
-              runHook preInstall
-
-              # this is the important line
-              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
-
-              mkdir -p $out/share
-              cp -a dist $out/share/jellyfin-web
-
-              runHook postInstall
-            '';
-          });
-        }
-    )
-  ];
 
 }
