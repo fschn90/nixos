216370d566991c679a2489d395c1db93b1530446
fschn90 <79014934+fschn90@users.noreply.github.com>
2026-09-18 23:49:05 +0200

    cleanup: ssh module

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,9 +1,6 @@
 {
-
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = false;
   services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.GatewayPorts = "yes";
-  # services.openssh.settings.X11Forwarding = true;
-
 }
